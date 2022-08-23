c83771c0a1919003a2bddbc7dc7fa9997d25b059
Junior Bugnaro <juninho.bugnaro@hotmail.com>
2022-08-23 19:57:46 -0300

    Update script.js

--- exercicios/desafio/02/ex/script.js
+++ exercicios/desafio/02/ex/script.js
@@ -3,7 +3,8 @@ function carregar() {
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} horas.`
+    var min = data.getMinutes()
+    msg.innerHTML = `Agora são ${hora}:${min} horas.`
     if (hora >= 0 && hora < 12) {
         img.src = 'imagens/manha.png'
         document.body.style.background = '#e2cd9f'
